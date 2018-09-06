c58a902d0929e7d643305bb689ea5f800e48ff48
rainfiel <rainfiel@gmail.com>
2018-09-06 21:01:24 +0800

    no border

--- script/border.lua
+++ script/border.lua
@@ -5,7 +5,7 @@ local geo = require "ejoy2d.geometry"
 
 local M = {}
 local border_render = render:create(1000, "border")
-border_render:show(M, render.top_left)
+-- border_render:show(M, render.top_left)
 
 function M:draw()
 	local tl = render:anchor(render.top_left)
@@ -14,20 +14,20 @@ function M:draw()
 	local dr = render:anchor(render.bottom_right)
 
 	for i=-1, 1 do
-		geo.line(tl.x, tl.y+i, tr.x, tr.y+i, 0xFFFF0000)
+		geo.line(tl.x, tl.y+i, tr.x, tr.y+i, 0xaa888888)
 	end
 	for i=-1, 1 do
-		geo.line(tl.x+i, tl.y, dl.x+i, dl.y, 0xFFFF0000)
+		geo.line(tl.x+i, tl.y, dl.x+i, dl.y, 0xaa888888)
 	end
 	for i=-1, 1 do
-		geo.line(dl.x, dl.y+i, dr.x, dr.y+i, 0xFFFF0000)
+		geo.line(dl.x, dl.y+i, dr.x, dr.y+i, 0xaa888888)
 	end
 	for i=-1, 1 do
-		geo.line(tr.x+i, tr.y, dr.x+i, dr.y, 0xFFFF0000)
+		geo.line(tr.x+i, tr.y, dr.x+i, dr.y, 0xaa888888)
 	end
 
-	geo.box(0, 0, tl.x-1, dl.y-1, 0xaa444444)
-	geo.box(tr.x+1, tr.y, fw.GameInfo.width - tr.x - 1, dl.y-1, 0xaa444444)
+	geo.box(0, 0, tl.x-1, dl.y, 0xaa444444)
+	geo.box(tr.x+1, tr.y, fw.GameInfo.width - tr.x - 1, dl.y, 0xaa444444)
 end
 
 return {
